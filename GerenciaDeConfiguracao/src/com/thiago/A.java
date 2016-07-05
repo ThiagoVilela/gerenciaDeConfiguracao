0652843304dfb29be497d0e36588210e6c617836
Thiago Vilela <thiagovilela@petasoft.com>
2016-07-04 22:23:11 -0300

    Versão - 1.0

---
+++ GerenciaDeConfiguracao/src/com/thiago/A.java
@@ -0,0 +1,24 @@
+package com.thiago;
+
+public class A {
+	//Atributos
+	private int A1;
+	private float A2;
+	
+	//Métodos
+	
+	//Getters and Setters
+	public int getA1() {
+		return A1;
+	}
+	public void setA1(int a1) {
+		A1 = a1;
+	}
+	public float getA2() {
+		return A2;
+	}
+	public void setA2(float a2) {
+		A2 = a2;
+	}
+	
+}
